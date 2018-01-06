14571fe0a0fd0f843bc2e68321a5cab97aeb9807
Patric Campbell <patric@patriccampbell.com>
2018-01-06 13:09:19 -0500

    Add enzyme for testing

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,14 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import App from "./App";
+import { shallow } from "enzyme";
 
 it("renders without crashing", () => {
-  const div = document.createElement("div");
-  ReactDOM.render(<App />, div);
+  shallow(<App />);
+});
+
+it("renders the welcome", () => {
+  const wrapper = shallow(<App />);
+  const welcome = <h1>Welcome to RollyDos!</h1>;
+  expect(wrapper.contains(welcome)).toEqual(true);
 });
